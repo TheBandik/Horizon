8b7898d19b1f8bab7c97ae5d5092f56fe0ec6a4d
Arkadiy Schneider <shnr.arky@gmail.com>
2026-01-20 20:01:47 +1000

    git status git branch --show-current fix frontend docker build

--- src/backend/src/main/java/thebandik/horizon/backend/config/SecurityConfig.java
+++ src/backend/src/main/java/thebandik/horizon/backend/config/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:5173",
                 "http://127.0.0.1:5173",
-"https://horizon.thebandik.com"
+                "https://horizon.thebandik.com"
         ));
 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
